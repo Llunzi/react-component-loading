87793c3cad4c491cf1d3f9512523a6234de97cb0
刘光伦 <liuguanglun@xiaoneng.cn>
2018-09-02 20:26:40 +0800

    init

---
+++ Loading.js
@@ -0,0 +1,27 @@
+import "loading.less";
+
+export default function loadingCircle()
+{
+	return (
+		<div className="xn-box">
+			<div className="xn-box-container container1">
+				<div className="circle1"></div>
+				<div className="circle2"></div>
+				<div className="circle3"></div>
+				<div className="circle4"></div>
+			</div>
+			<div className="xn-box-container container2">
+				<div className="circle1"></div>
+				<div className="circle2"></div>
+				<div className="circle3"></div>
+				<div className="circle4"></div>
+			</div>
+			<div className="xn-box-container container3">
+				<div className="circle1"></div>
+				<div className="circle2"></div>
+				<div className="circle3"></div>
+				<div className="circle4"></div>
+			</div>
+		</div>
+	);
+}
